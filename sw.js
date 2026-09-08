70120eaee012104f9ec67016b48ed599cce31da6
mpvinnye-lab <mpvinnye@gmail.com>
2026-09-08 16:17:35 -0400

    Sweetify v7: atualiza cache PWA

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='doce-caixa-v6';
+const CACHE='doce-caixa-v7';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
